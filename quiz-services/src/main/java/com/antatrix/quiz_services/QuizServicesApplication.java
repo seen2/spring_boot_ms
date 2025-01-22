2d0dadd7843add8b3fdde375a2d889116a345c68
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2025-01-22 14:32:50 +0000

    quiz services

---
+++ quiz-services/src/main/java/com/antatrix/quiz_services/QuizServicesApplication.java
@@ -0,0 +1,13 @@
+package com.antatrix.quiz_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizServicesApplication.class, args);
+	}
+
+}
