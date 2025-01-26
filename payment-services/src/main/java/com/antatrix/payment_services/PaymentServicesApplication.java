cf0ffa53c381c04e6a2149f1a919887207bda69f
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2025-01-26 13:05:50 +0000

    base start

---
+++ payment-services/src/main/java/com/antatrix/payment_services/PaymentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.antatrix.payment_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServicesApplication.class, args);
+	}
+
+}
